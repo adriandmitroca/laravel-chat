b62bfd4363b2ea26fac601b597439593b5695b4a
Adrian Dmitroca <dmitroca.adrian@gmail.com>
2016-02-01 20:09:55 +0100

    Speed up transitions, send messages with enter

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,10 +12,10 @@ var pusher = new Pusher('3c511981ee118763016d', {
 var chat = pusher.subscribe('chat');
 
 chat.bind('App\\Events\\MessageWasSent', function (data) {
-    $('#messages').append($('<p><strong>' + data.message.author + '</strong> [' + data.message.created_at + ']: ' + data.message.content + '</p>').hide().fadeIn(2000));
+    $('#messages').append($('<p><strong>' + data.message.author + '</strong> [' + data.message.created_at + ']: ' + data.message.content + '</p>').hide().fadeIn(500));
 
     if ($('#messages p').length > 10) {
-        $('#messages p:first-of-type').fadeOut(2000).remove();
+        $('#messages p:first-of-type').fadeOut(500).remove();
     }
 });
 
@@ -26,4 +26,11 @@ $('#sendMessage').submit(function (e) {
     $.post('chat/new', vm.serialize()).done(function () {
         vm.find('textarea').val('');
     });
+});
+
+$('textarea').keypress(function (e) {
+    if (e.which == 13) {
+        $(this).closest('form').submit();
+        return false;
+    }
 });
